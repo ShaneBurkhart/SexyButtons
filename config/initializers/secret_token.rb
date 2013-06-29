d47244295de0c2da3c444e9745a571e5b7ef09c4
ShaneBurkhart <shaneburkhart@gmail.com>
2013-06-29 14:02:07 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SexyButtons::Application.config.secret_token = '6d386797fa98a7359d053e46b0f011898d715271de3906bd1187d53a6502297c427474f40578ba520051f6fbd207d2cc50a910fed21300d1d6f84f495eeaf60f'
